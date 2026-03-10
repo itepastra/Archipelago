a47002ade9610b97177930f7c484ac6607c05d9d
Noa Aarts <noa@voorwaarts.nl>
2026-03-10 13:17:54 +0100

    when endgame is included, make return scepter valid for purple shorts

--- worlds/stardew_valley/logic/special_items_logic.py
+++ worlds/stardew_valley/logic/special_items_logic.py
@@ -20,11 +20,15 @@ class SpecialItemsLogicMixin(BaseLogicMixin):
 
 class SpecialItemsLogic(BaseLogic):
 
-    def has_purple_shorts(self) -> StardewRule:
+    def has_purple_shorts(self, scepter_is_logic: bool) -> StardewRule:
         has_first_shorts = self.logic.season.has(Season.summer) &\
                            self.logic.region.can_reach(Region.ranch) &\
                            self.logic.relationship.has_hearts(NPC.marnie, 2)
-        has_repeatable_shorts = self.logic.region.can_reach(Region.purple_shorts_maze) & self.logic.has(Consumable.warp_totem_farm)
+        if scepter_is_logic:
+            can_warp_back = self.logic.has(Consumable.warp_totem_farm) | self.logic.received("Return Scepter")
+        else:
+            can_warp_back = self.logic.has(Consumable.warp_totem_farm)
+        has_repeatable_shorts = self.logic.region.can_reach(Region.purple_shorts_maze) & can_warp_back
         return has_first_shorts & has_repeatable_shorts
 
     def has_far_away_stone(self) -> StardewRule:
