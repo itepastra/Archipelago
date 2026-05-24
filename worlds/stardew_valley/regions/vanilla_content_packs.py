38231af4316b4ef83e988253260d8fc52f4a6dd0
Noa Aarts <noa@voorwaarts.nl>
2026-05-24 15:51:30 +0200

    fix volcano 5 to island north not entrance randomizing

--- worlds/stardew_valley/regions/vanilla_content_packs.py
+++ worlds/stardew_valley/regions/vanilla_content_packs.py
@@ -406,7 +406,7 @@ ginger_island_connections = [
     ConnectionData(
         Entrance.volcano_5_to_island_north,
         Region.island_north,
-        flag=RandomizationFlag.BUILDINGS | RandomizationFlag.IS_ONE_WAY,
+        flag=RandomizationFlag.BUILDINGS | RandomizationFlag.IS_ONE_WAY | GroupFlag.DOOR | GroupFlag.IN_TO_OUT,
     ),
     ConnectionData(Entrance.climb_to_volcano_10, Region.volcano_floor_10),
     ConnectionData(
