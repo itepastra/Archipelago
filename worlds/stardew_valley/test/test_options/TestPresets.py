cfd326cf7db5cc1df8c3598bbbb322ace8998faf
Alex Gilbert <alexgilbert@yahoo.com>
2026-06-15 15:48:34 -0400

    - Fixed more bugs

--- worlds/stardew_valley/test/test_options/TestPresets.py
+++ worlds/stardew_valley/test/test_options/TestPresets.py
@@ -1,5 +1,5 @@
-from Options import PerGameCommonOptions, OptionSet, OptionDict
-from ...options import StardewValleyOptions, TrapItems, BundlePlando
+from Options import PerGameCommonOptions, OptionSet, OptionDict, Removed
+from ...options import StardewValleyOptions
 from ...options.presets import sv_options_presets
 from ...test.bases import SVTestCase
 
@@ -7,10 +7,10 @@ from ...test.bases import SVTestCase
 class TestPresets(SVTestCase):
     def test_all_presets_explicitly_set_all_options(self):
         all_option_names = {option_key for option_key in StardewValleyOptions.type_hints}
-        omitted_option_names = {option_key for option_key in PerGameCommonOptions.type_hints} | {TrapItems.internal_name, BundlePlando.internal_name}
+        omitted_option_names = {option_key for option_key in PerGameCommonOptions.type_hints}
         mandatory_option_names = {option_key for option_key in all_option_names
                                   if option_key not in omitted_option_names and
-                                  not issubclass(StardewValleyOptions.type_hints[option_key], OptionSet | OptionDict)}
+                                  not issubclass(StardewValleyOptions.type_hints[option_key], OptionSet | OptionDict | Removed)}
 
         for preset_name in sv_options_presets:
             with self.subTest(f"{preset_name}"):
